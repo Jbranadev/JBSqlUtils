90d8bd5712cd50e08853efd67e87c482a58a4efb
jobran <jobran@gtc.com.gt>
2022-07-29 16:47:16 -0600

    Creación de primeros metodos

--- src/main/java/io/github/josecarlosbran/JBSqlLite/Methods.java
+++ src/main/java/io/github/josecarlosbran/JBSqlLite/Methods.java
@@ -4,6 +4,8 @@ import io.github.josecarlosbran.LogsJB.LogsJB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Methods {
 /*
@@ -47,6 +49,43 @@ try{
             LogsJB.fatal("Mensaje de la Excepción : "+e.getMessage());
             LogsJB.fatal("Trace de la Excepción : "+e.getStackTrace());
         }
+    }
+
+
+
+
+    //Obtener un objeto generico de una lista
+    public static <T> T getObject(int id, List<T> lista){
+        return lista.get(id);
+    }
+
+    //agregar un objeto generico de una lista
+    public static <T> void addDato(List<T> lista, T dato){
+        lista.add(dato);
+    }
+
+    public static <T> List<T> getall(T a, T b, T c){
+        List<T> lista=new ArrayList<>();
+        lista.add(a);
+        lista.add(b);
+        lista.add(c);
+        return lista;
+    }
+
+    public static void main(String[] args){
+        List<String> cadenas= new ArrayList<>();
+        addDato(cadenas, "Jose");
+        addDato(cadenas, "Carlos");
+        addDato(cadenas, "Alfredo");
+        addDato(cadenas, "Bran");
+        addDato(cadenas, "Aguirre");
+        List<Integer> numeros= new ArrayList<>();
+        numeros.add(50);
+        numeros.add(100);
+        System.out.println("primer numero de la lista: "+getObject(0, numeros));
+        System.out.println("3 Cadena de la lista: "+getObject(2, cadenas));
+        getall(1, 5, 10).forEach(System.out::println);
+
     }
 
 }
