3638edec1f8c347761c8c441bbd701ba65f7a98f
Jose Bran <jobran@gtc.com.gt>
2024-08-20 08:11:38 -0600

    Revisión Avances de Mayron
    *No sea chambon jajajajajaj

--- src/main/java/io/github/josecarlosbran/JBSqlUtils/Adaptadores/DateAdapter.java
+++ src/main/java/io/github/josecarlosbran/JBSqlUtils/Adaptadores/DateAdapter.java
@@ -1,6 +1,8 @@
 package io.github.josecarlosbran.JBSqlUtils.Adaptadores;
 
+import com.josebran.LogsJB.LogsJB;
 import jakarta.json.bind.adapter.JsonbAdapter;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
@@ -20,7 +22,7 @@ public class DateAdapter implements JsonbAdapter<Date, String> {
      * el formato en que deseen manejar la fecha, sin afectar a los demas hilos.
      * se esta seteando o estableciendo el threadLocalDateFormat al new SimpleDateFormat(pattern)
      *
-     * @param pattern
+     * @param pattern Es el patron que se desea establecer.
      */
     public static void setDateFormatPattern(String pattern) {
         threadLocalDateFormat.set(new SimpleDateFormat(pattern));
@@ -31,13 +33,12 @@ public class DateAdapter implements JsonbAdapter<Date, String> {
      * este metodo devuelve o retorna un String.
      * y puede lanzar una excepcion en caso que algo salga mal.
      *
-     * @param date
-     * @return
-     * @throws Exception
+     * @param date Es el objeto de tipo Date que se desea convertir a un String.
+     * @return Retorna un String que representa la fecha recibida como parametro
      */
 
     @Override
-    public String adaptToJson(Date date) throws Exception {
+    public String adaptToJson(Date date) {
         //aqui se obtiene el dato de Fecha del hilo actual.
         SimpleDateFormat dateFormat = threadLocalDateFormat.get();
         return dateFormat.format(date);
@@ -46,15 +47,20 @@ public class DateAdapter implements JsonbAdapter<Date, String> {
     /**
      * Este metodo es el que convierte las cadenas en objetos tipo Date.
      *
-     * @param dateString
-     * @return
-     * @throws Exception
+     * @param dateString Es la cadena que se desea convertir a un objeto tipo Date.
+     * @return Retorna un objeto tipo Date con la fecha que se recibio como parametro.
      */
     @Override
-    public Date adaptFromJson(String dateString) throws Exception {
-        //aqui se obtiene el dato de Fecha del hilo actual.
-        SimpleDateFormat dateFormat = threadLocalDateFormat.get();
-        //en esta linea se parsea la cadena de fecha, y se convierte en un objeto tipo Date
-        return new Date(dateFormat.parse(dateString).getTime());
+    public Date adaptFromJson(String dateString) {
+        try {
+            //aqui se obtiene el dato de Fecha del hilo actual.
+            SimpleDateFormat dateFormat = threadLocalDateFormat.get();
+            //en esta linea se parsea la cadena de fecha, y se convierte en un objeto tipo Date
+            return new Date(dateFormat.parse(dateString).getTime());
+        } catch (Exception e) {
+            LogsJB.fatal("Error capturado al convertir la fecha: " + dateString);
+            LogsJB.fatal("Stacktrace de la excepción: " + ExceptionUtils.getStackTrace(e));
+            return null;
+        }
     }
 }
